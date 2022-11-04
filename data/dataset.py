63a8f419d8cd65a4318289827386c6b6e3a71678
danielsyahputra13 <dnlshp@gmail.com>
2022-11-04 07:55:59 +0700

    add dataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,7 +1,55 @@
 import os
 import torch
+import pickle
+import numpy as np
 from torch.utils.data import Dataset
+from typing import Tuple
+from PIL import Image
 
 class PA100KDataset(Dataset):
-    def __init__(self, root: str, label, transform = None) -> None:
+    def __init__(self, 
+                dataset_path: str,
+                partition_path: str,
+                split: str = 'train',
+                partition_idx: int = 0,
+                transform = None, **kwargs) -> None:
         super().__init__()
+        if os.path.exists(dataset_path):
+            self.dataset = pickle.load(open(dataset_path, "rb"))
+        else:
+            print(f"{dataset_path} doesn't exist!")
+            raise ValueError
+
+        if os.path.exists(partition_path):
+            self.partition = pickle.load(open(partition_path, "rb"))
+        else:
+            print(f"{partition_path} doesn't exist!")
+            raise ValueError
+        
+        if not self.partition.has_key(split):
+            print(f"Split {split} does not exist in dataset!")
+            raise ValueError
+        if partition_idx > len(self.partition[split]) - 1:
+            print(f"Partition idx: {partition_idx} is out of range in partition!")
+            raise ValueError
+    
+        self.transform = transform
+        self.root = self.dataset['root']
+        self.att_name = [self.dataset['att_name'][i] for i in self.dataset['selected_attribute']]
+        self.images = []
+        self.labels = []
+        for idx in self.partition[split][partition_idx]:
+            self.images.append(self.dataset["image"][idx])
+            label_tmp = np.array(self.dataset['att'][idx])[self.dataset['selected_attribute']].tolist()
+            self.label.append(label_tmp)
+
+    def __getitem__(self, index) -> Tuple:
+        image, target = self.images[index], self.labels[image]
+        image_path = os.path.join(self.dataset["root"], image)
+        img = Image.open(image_path)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.images)
