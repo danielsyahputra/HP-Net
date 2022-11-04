d6c9bb747e873964eff26e1265b68d37e37154ba
danielsyahputra13 <dnlshp@gmail.com>
2022-11-04 13:45:22 +0700

    implement MainNet

--- models/mnet.py
+++ models/mnet.py
@@ -26,9 +26,9 @@ class InceptionA(nn.Module):
                 base_1x1_out,
                 base_3x3_1_out,
                 base_3x3_2_out,
-                base_5x5_1_out,
-                base_5x5_2_out,
-                base_5x5_3_out,
+                base_3x3dbl_1_out,
+                base_3x3dbl_2_out,
+                base_3x3dbl_3_out,
                 base_pool_out) -> None:
         
         # Note: You can refer to Inception-V2 architecture from the papers that I mentioned above.
@@ -37,12 +37,12 @@ class InceptionA(nn.Module):
         self.branch1x1 = ConvBlock(in_channels, base_1x1_out, kernel_size=1)
         self.branch3x3_1 = ConvBlock(in_channels, base_3x3_1_out, kernel_size=1)
         self.branch3x3_2 = ConvBlock(base_3x3_1_out, base_3x3_2_out, kernel_size=3, padding=1)
-        self.branch3x3dbl_1 = ConvBlock(in_channels, base_5x5_1_out, kernel_size=1)
-        self.branch3x3dbl_2 = ConvBlock(base_5x5_1_out, base_5x5_2_out, kernel_size=3, padding=1)
-        self.branch3x3dbl_3 = ConvBlock(base_5x5_2_out, base_5x5_3_out, kernel_size=3, padding=1)
+        self.branch3x3dbl_1 = ConvBlock(in_channels, base_3x3dbl_1_out, kernel_size=1)
+        self.branch3x3dbl_2 = ConvBlock(base_3x3dbl_1_out, base_3x3dbl_2_out, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = ConvBlock(base_3x3dbl_2_out, base_3x3dbl_3_out, kernel_size=3, padding=1)
         self.branch_pool = ConvBlock(in_channels, base_pool_out, kernel_size=1)
 
-    def forward(self, x):
+    def forward(self, x) -> Tensor:
         branch1x1 = self.branch1x1(x)
         branch3x3 = self.branch3x3_1(x)
         branch3x3 = self.branch3x3_2(branch3x3)
@@ -53,6 +53,72 @@ class InceptionA(nn.Module):
         branch_pool = self.branch_pool(branch_pool)
         outputs = [branch1x1, branch3x3, branch3x3dbl, branch_pool]
         return torch.cat(outputs, 1)
+
+class InceptionB(nn.Module):
+    def __init__(self,
+                in_channels,
+                base_1x1_1_out,
+                base_3x3_1_out,
+                base_3x3dbl_1_out,
+                base_3x3dbl_2_out,
+                base_3x3dbl_3_out) -> None:
+        super().__init__()
+        self.branch3x3_1 = ConvBlock(in_channels, base_1x1_1_out, kernel_size=1)
+        self.branch3x3_2 = ConvBlock(base_1x1_1_out, base_3x3_1_out, kernel_size=3, stride=2, padding=1)
+
+        self.branch3x3dbl_1 = ConvBlock(in_channels, base_3x3dbl_1_out, kernel_size=1)
+        self.branch3x3dbl_2 = ConvBlock(base_3x3dbl_1_out, base_3x3dbl_2_out, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = ConvBlock(base_3x3dbl_2_out, base_3x3dbl_3_out, kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x) -> Tensor:
+        branch3x3 = self.branch3x3_1(x)
+        branch3x3 = self.branch3x3_2(branch3x3)
+
+        branch3x3dbl = self.branch3x3dbl_1(x)
+        branch3x3dbl = self.branch3x3dbl_2(branch3x3dbl)
+        branch3x3dbl = self.branch3x3dbl_3(branch3x3dbl)
+        branch_pool = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        outputs = [branch3x3, branch3x3dbl, branch_pool]
+        return torch.cat(outputs,dim=1)
+
+class InceptionBlock1(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.module1 = InceptionA(in_channels=96, base_1x1_out=32, base_3x3_1_out=32, base_3x3_2_out=32,
+                                base_3x3dbl_1_out=32, base_3x3dbl_2_out=48, base_3x3dbl_3_out=48, base_pool_out=16)
+        self.module2 = InceptionB(in_channels=128, base_1x1_1_out=64, base_3x3_1_out=80,
+                                base_3x3dbl_1_out=32, base_3x3dbl_2_out=48, base_3x3dbl_3_out=48)
+
+    def forward(self, x) -> Tensor:
+        x = self.module1(x)
+        x = self.module2(x)
+        return x
+
+class InceptionBlock2(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.module1 = InceptionA(in_channels=256, base_1x1_out=112, base_3x3_1_out=32, base_3x3_2_out=48,
+                                base_3x3dbl_1_out=48, base_3x3dbl_2_out=64, base_3x3dbl_3_out=64, base_pool_out=64)
+        self.module2 = InceptionB(in_channels=288, base_1x1_1_out=64, base_3x3_1_out=86,
+                                base_3x3dbl_1_out=96, base_3x3dbl_2_out=128, base_3x3dbl_3_out=128)
+
+    def forward(self, x) -> Tensor:
+        x = self.module1(x)
+        x = self.module2(x)
+        return x
+
+class InceptionBlock3(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.module1 = InceptionA(in_channels=502, base_1x1_out=176, base_3x3_1_out=96, base_3x3_2_out=160,
+                                base_3x3dbl_1_out=80, base_3x3dbl_2_out=112, base_3x3dbl_3_out=112, base_pool_out=64)
+        self.module2 = InceptionA(in_channels=512, base_1x1_out=176, base_3x3_1_out=96, base_3x3_2_out=160,
+                                base_3x3dbl_1_out=96, base_3x3dbl_2_out=112, base_3x3dbl_3_out=112, base_pool_out=64)
+
+    def forward(self, x) -> Tensor:
+        x = self.module1(x)
+        x = self.module2(x)
+        return x
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs) -> None:
@@ -65,10 +131,3 @@ class ConvBlock(nn.Module):
     def forward(self, x):
         x = self.block(x)
         return F.relu(x, inplace=True)
-
-class InceptionBlock1(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-    
-    def forward(self, x):
-        pass
