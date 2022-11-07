b257b9329e271508dec9e296f3395c8aeb731c23
danielsyahputra13 <dnlshp@gmail.com>
2022-11-07 14:43:08 +0700

    add hydraPlusNet

---
+++ models/hydraplus.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mnet import MainNet
+from afnet import AFNet
+
+class HydraPlusNet(nn.Module):
+    def __init__(self, num_classes: int = 26, att_out: bool = False) -> None:
+        super().__init__()
+        self.att_out = att_out
+        with torch.inference_mode():
+            if self.att_out:
+                self.main_net = MainNet(feat_out=True)
+                self.af1 = AFNet(att_out=True, feat_out=True, af_name="AF1")
+                self.af2 = AFNet(att_out=True, feat_out=True, af_name="AF2")
+                self.af3 = AFNet(att_out=True, feat_out=True, af_name="AF3")
+            else:
+                self.main_net = MainNet(feat_out=True)
+                self.af1 = AFNet(feat_out=True, af_name="AF1")
+                self.af2 = AFNet(feat_out=True, af_name="AF2")
+                self.af3 = AFNet(feat_out=True, af_name="AF3")
+        self.fc = nn.Linear(512 * 73, num_classes)
+
+    def forward(self, x):
+        _, _, _, feature0 = self.main_net(x)
+        if self.att_out:
+            feature1, att1 = self.af1(x)
+            feature2, att2 = self.af2(x)
+            feature3, att3 = self.af3(x)
+        else:
+            feature1 = self.af1(x)
+            feature2 = self.af2(x)
+            feature3 = self.af3(x)
+
+        ret = torch.cat((feature0, feature1, feature2, feature3), dim=1)
+        fc_input = F.avg_pool2d(ret, kernel_size=9, stride=1)
+        fc_input = F.dropout(fc_input, training=self.training)
+        fc_input = fc_input.view(fc_input.size(0), -1)
+        ret = self.fc(fc_input)
+        if self.att_out:
+            return att1, att2, att3, ret
+        return ret
